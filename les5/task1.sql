7ef6f57bb02255ec1c2a726c723a48b3dc0a42d7
vilesovds <vilesovds@gmail.com>
2021-02-09 00:24:02 +0300

    renamed to sql

--- les5/task1.sql
+++ les5/task1.sql
@@ -1,7 +1,7 @@
-UPDATE users
+﻿UPDATE users
 SET created_at = NOW(), updated_at = NOW();
 
-Но можно добавить проверку на NULL, для этого придётся делать два запроса
+/*Но можно добавить проверку на NULL, для этого придётся делать два запроса*/
 
 UPDATE users
 SET created_at = NOW()
